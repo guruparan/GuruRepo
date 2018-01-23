6149368f1cf6834aeaf7a1dd51857692db314bf4
Guruparan Giritharan <guru_sig@yahoo.com>
2018-01-23 17:35:58 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,10 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every three minutes.')
+    print(os.environ.get('TestVal', None))
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
