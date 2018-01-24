6d45ac5cdfcb99a2cefc0bb865044f27e4197d32
Guruparan Giritharan <guru_sig@yahoo.com>
2018-01-24 11:58:58 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,13 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
+from DataAccess import DataAccess
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
-    print('This job is run every three minutes.')
-    print(os.environ.get('TestVal', None))
-
+    db=DataAccess()
+    db.execute("insert into appuser(name) values('testyser')")
+    
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
